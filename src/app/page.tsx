fa299aa2d0761cadcbf043258d333f0caeb80570
hanbu97 <hanbu97@proton.me>
2023-12-23 20:53:08 +0800

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,41 +32,6 @@ export default function Home() {
   }, []);
 
 
-  // useEffect(() => {
-  //   const initAuthClient = async () => {
-  //     const newAuthClient = await AuthClient.create();
-  //     setAuthClient(newAuthClient);
-  //   };
-
-  //   initAuthClient();
-
-  //   // whoAmI();
-  // }, []);
-
-  // const logout = async () => {
-  //   await authClient?.logout();
-
-  //   const tmpIdentity = authClient?.getIdentity();
-  //   setIdentity(tmpIdentity);
-
-  //   const identity = client.getIdentity();
-  //   setIdentity(identity);
-
-  //   const principal = identity.getPrincipal();
-  //   setPrincipal(principal);
-
-  //   setAuthClient(client);
-
-  //   const actor = createActor(canisterId, {
-  //     agentOptions: {
-  //       identity,
-  //     },
-  //   });
-
-  //   whoAmI();
-  //   console.log(principal);
-  // }
-
   const logout = () => {
     authClient?.logout().then(() => {
       updateClient(authClient!);
@@ -111,35 +76,6 @@ export default function Home() {
     });
   };
 
-  // const handleLogin = async () => {
-  //   if (authClient) {
-  //     await new Promise<void>((resolve) => {
-  //       authClient.login({
-  //         identityProvider: process.env.DFX_NETWORK === "ic"
-  //           ? "https://identity.ic0.app"
-  //           : `http://localhost:4943/?canisterId=br5f7-7uaaa-aaaaa-qaaca-cai`,
-  //         onSuccess: () => resolve(),
-  //       });
-  //     });
-
-  //     const identity = authClient.getIdentity();
-  //     initialize(identity); // 使用新的身份初始化
-
-  //     // 重新初始化全局 actor 实例
-  //     const agent = new HttpAgent({ identity });
-  //     const canisterId = process.env.CANISTER_ID_HELLO;
-
-  //     if (!canisterId) {
-  //       throw new Error("CANISTER_ID_HELLO environment variable is not defined.");
-  //     }
-
-  //     // const newActor = createActor(canisterId, { agent });
-  //     // 这里可能不需要显式设置 actor，因为 initialize 应该已经更新了全局状态
-
-  //     // whoAmI();
-  //     console.log(principal);
-  //   }
-  // };
 
   const testfn = async () => {
     const identity = authClient?.getIdentity();
